9b987dea6fffdf70df63dc15ff04082f299b5e83
MUHAMMADhad <muhammadhadiofficial123@gmail.com>
2025-12-07 22:50:34 +0500

    deletec files

--- backend/src/controllers/attendance.controller.js
+++
@@ -1,9 +0,0 @@
-export const markAttendance = async (req, res) => res.json({
-    message: 'Route working'
-});
-export const getClassAttendance = async (req, res) => res.json({
-    message: 'Route working'
-});
-export const getMyAttendance = async (req, res) => res.json({
-    message: 'Route working'
-});
